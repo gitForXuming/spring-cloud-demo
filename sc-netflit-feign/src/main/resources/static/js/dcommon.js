f7f1e35e805b9434cce5e9343d182f62eaaeb39e
gitForXuming <xuming_1024@163.com>
2018-06-01 16:39:23 +0800

    contextPath

--- sc-netflit-feign/src/main/resources/static/js/dcommon.js
+++ sc-netflit-feign/src/main/resources/static/js/dcommon.js
@@ -11,19 +11,18 @@ demo.common = function () {
 
     return {
         init : function () {
-            this.buildFooter();
             demo.lang.init();
         },
-        buildHead : function (index) {
-            $.get('/demo/header.html' ,function (html) { //发起一个http请求到 header.html并拿到结果值 这里是拿html $.get("/try/ajax/demo_test.php",function(data,status)
+        buildHead : function (contextPath,index) {
+            $.get(contextPath+'/header.html' ,function (html) { //发起一个http请求到 header.html并拿到结果值 这里是拿html $.get("/try/ajax/demo_test.php",function(data,status)
                 $(document.body).prepend(html);//将html 追加到当前页面body的最前面
                 demo.lang.trigger();
                 $(".navbar .nav li").eq(index).addClass("active");
             },"html");
 
         },
-        buildFooter : function() {
-            $.get('/demo/footer.html' ,function (html) { //发起一个http请求到 header.html并拿到结果值 这里是拿html $.get("/try/ajax/demo_test.php",function(data,status)
+        buildFooter : function(contextPath) {
+            $.get(contextPath+'/footer.html' ,function (html) { //发起一个http请求到 header.html并拿到结果值 这里是拿html $.get("/try/ajax/demo_test.php",function(data,status)
                 $(document.body).append(html);//将html 追加到当前页面body的最前面
             },"html");
         },
